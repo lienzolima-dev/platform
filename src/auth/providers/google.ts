c0df6b11e96c52fed97125be2c6fe39caf90c5be
Fernando Vegas <90532274+Thgear27@users.noreply.github.com>
2024-10-23 00:38:24 -0500

    use dynamic callback URL

--- src/auth/providers/google.ts
+++ src/auth/providers/google.ts
@@ -6,8 +6,11 @@ const GOOGLE_CLIENT_SECRET = import.meta.env.GOOGLE_CLIENT_SECRET;
 if (!GOOGLE_CLIENT_ID) throw new Error("GOOGLE_CLIENT_ID is not set");
 if (!GOOGLE_CLIENT_SECRET) throw new Error("GOOGLE_CLIENT_SECRET is not set");
 
+const PROD = import.meta.env.PROD;
+const base = PROD ? "https://lienzolima.com" : "http://localhost:4321";
+
 export const google = new Google(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
-  "http://localhost:4321/login/google/callback",
+  base.concat("/login/google/callback"),
 );
