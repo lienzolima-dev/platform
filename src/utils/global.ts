f9d09506a24537a2e216945a79aefabebdf887db
Fernando Vegas <90532274+Thgear27@users.noreply.github.com>
2024-10-31 11:08:39 -0500

    feat: update environment variables

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,4 +1,4 @@
+import { getWebsiteUrl } from "./env";
+
 export const instagramDirectMessageUrl = "https://ig.me/m/lienzo.lima/";
-export const baseUrl = import.meta.env.PROD
-  ? "https://lienzolima.com"
-  : "http://localhost:4321";
+export const baseUrl = getWebsiteUrl();
