2609ee2c939d1f5407219fa7b4afd7b7e3cb460c
Fernando Vegas <90532274+Thgear27@users.noreply.github.com>
2024-10-31 11:12:36 -0500

    feat: update environment variables (#19)

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -14,7 +14,15 @@ export function getGoogleClientId(): string {
 
 export function getGoogleClientSecret(): string {
   const googleClientSecret = import.meta.env.GOOGLE_CLIENT_SECRET;
+  console.log("googleClientSecret", googleClientSecret);
   if (!googleClientSecret) throw new Error("GOOGLE_CLIENT_SECRET is not set");
 
   return googleClientSecret;
+}
+
+export function getWebsiteUrl(): string {
+  const websiteUrl = import.meta.env.WEBSITE_URL;
+  if (!websiteUrl) throw new Error("WEBSITE_URL is not set");
+
+  return websiteUrl;
 }
