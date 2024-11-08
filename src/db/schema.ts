e293c8020de45addc3a6a5b601a61be0d727d84a
Alexia Asunción <alexiaap_304@hotmail.com>
2024-11-07 19:29:55 -0500

    fix format

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
 export { users } from "./schemas/users";
 export { sessions } from "./schemas/sessions";
-export {tasks} from "./schemas/tasks";
-export {bookings} from "./schemas/bookings";
+export { tasks } from "./schemas/tasks";
+export { bookings } from "./schemas/bookings";
 export { services } from "./schemas/services";
 export { extras } from "./schemas/extras";
 export { bookingsServicesDetails } from "./schemas/bookings_services_details";
