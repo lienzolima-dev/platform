19c62fc8d339487d775920cc92ea2024e5b75597
Alexia Asunción <alexiaap_304@hotmail.com>
2024-11-08 03:38:07 -0500

    add seed data

--- src/db/seed/seed.ts
+++ src/db/seed/seed.ts
@@ -1,8 +1,28 @@
 import { adminsToSeed } from "./data";
 import { seedUser } from "./utils/seedUser";
+import { tasksToSeed } from "./data";
+import { seedTask } from "./utils/seedTask";
+import { manicuristsToSeed } from "./data";
+import { seedServices } from "./utils/seedServices";
+import { servicesToSeed } from "./data";
+import { seedExtras } from "./utils/seedExtras";
+import { extrasToSeed } from "./data";
+import { seedBookings } from "./utils/seedBookings";
+import { bookingsToSeed } from "./data";
+import { seedBookingsServicesDetails } from "./utils/seedBookingsServicesDetails";
+import { bookingsServicesDetailsToSeed } from "./data";
+import { seedBookingsExtrasDetails } from "./utils/seedBookingsExtrasDetails";
+import { bookingsExtrasDetailsToSeed } from "./data";
 
 async function seed() {
   await seedUser(adminsToSeed);
+  await seedUser(manicuristsToSeed);
+  await seedTask(tasksToSeed);
+  await seedServices(servicesToSeed);
+  await seedExtras(extrasToSeed);
+  await seedBookings(bookingsToSeed);
+  await seedBookingsServicesDetails(bookingsServicesDetailsToSeed);
+  await seedBookingsExtrasDetails(bookingsExtrasDetailsToSeed);
 }
 
 console.log("[LOG]: Seeding data...");
