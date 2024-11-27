73b6a8a06db9ea73cd71d72621a7ba2e6ab5370d
Fernando Vegas <90532274+Thgear27@users.noreply.github.com>
2024-11-27 15:47:05 -0500

    feat: add pagination for users, collaborators and bookings routes (#51)
    * add pagination for users route
    
    * add pagination for collaborator route
    
    * add pagination for bookings route
    
    * fix search

--- src/pages/admin/_db/bookings.ts
+++ src/pages/admin/_db/bookings.ts
@@ -1,5 +1,7 @@
+import { and, count, gte, lte } from "drizzle-orm";
 import { db } from "../../../db/db";
 import type { Booking } from "./types";
+import { bookings } from "../../../db/schema";
 
 function formatDateString(dateString: string): string {
   return new Date(dateString).toLocaleTimeString("es-PE", {
@@ -17,9 +19,31 @@ function formatPaymentStatus(paymentStatus: string): string {
   return paymentStatus;
 }
 
-export async function getAllBookings(): Promise<Booking[]> {
+export async function getBookings({
+  offset,
+  limit,
+  day,
+}: {
+  offset?: number;
+  limit?: number;
+  day?: Date;
+}): Promise<Booking[]> {
   const bookings = await db.query.bookings.findMany({
     orderBy: (bookings, { desc }) => [desc(bookings.startTime)],
+    offset: offset ?? 0,
+    limit: limit ?? 10,
+    where: (bookings, { gte, lte }) => {
+      if (day) {
+        const start = new Date(day);
+        start.setHours(0, 0, 0, 0);
+        const end = new Date(day);
+        end.setHours(23, 59, 59, 999);
+        return and(
+          gte(bookings.startTime, start.toISOString()),
+          lte(bookings.startTime, end.toISOString()),
+        );
+      }
+    },
     with: {
       bookingsExtrasDetails: {
         with: {
@@ -42,8 +66,8 @@ export async function getAllBookings(): Promise<Booking[]> {
       endTime: formatDateString(booking.endTime),
       date: new Date(booking.startTime),
       username: booking.name,
-      phone: booking.phone,
       email: booking.email,
+      phone: booking.phone,
       paymentStatus: formatPaymentStatus(booking.paymentStatus),
       services: booking.bookingsServicesDetails.map(
         (detail) => detail.services,
@@ -58,52 +82,75 @@ export async function getAllBookings(): Promise<Booking[]> {
   return formattedBookings;
 }
 
+export async function getAllBookings(): Promise<Booking[]> {
+  const bookings = await getBookings({});
+  return bookings;
+}
+
+export async function getBookingsCount({
+  day,
+}: {
+  day?: Date;
+}): Promise<number> {
+  let query = db
+    .select({
+      count: count(),
+    })
+    .from(bookings)
+    .$dynamic();
+
+  if (day) {
+    const start = new Date(day);
+    start.setHours(0, 0, 0, 0);
+    const end = new Date(day);
+    end.setHours(23, 59, 59, 999);
+
+    query = query.where(
+      and(
+        gte(bookings.startTime, start.toISOString()),
+        lte(bookings.startTime, end.toISOString()),
+      ),
+    );
+  }
+
+  const bookingsCount = await query;
+
+  return bookingsCount[0].count;
+}
+
+export async function getPaginatedBookings({
+  page,
+  pageSize,
+}: {
+  page: number;
+  pageSize: number;
+}): Promise<{
+  data: Booking[];
+  count: number;
+  totalPages: number;
+}> {
+  const offset = (page - 1) * pageSize;
+
+  const [bookings, bookingCount] = await Promise.all([
+    getBookings({ offset, limit: pageSize }),
+    getBookingsCount({}),
+  ]);
+
+  return {
+    data: bookings,
+    count: bookingCount,
+    totalPages: Math.ceil(bookingCount / pageSize),
+  };
+}
+
 export async function getUpcomingBookings(): Promise<Booking[]> {
   const now = new Date();
-  const todayStart = new Date(now.setHours(0, 0, 0, 0));
-  const todayEnd = new Date(now.setHours(23, 59, 59, 999));
 
-  const bookings = await db.query.bookings.findMany({
-    orderBy: (bookings, { desc }) => [desc(bookings.startTime)],
-    where: (bookings, { gte, lte, and }) =>
-      and(
-        gte(bookings.startTime, todayStart.toISOString()),
-        lte(bookings.startTime, todayEnd.toISOString()),
-      ),
-    with: {
-      bookingsExtrasDetails: {
-        with: {
-          extras: true,
-        },
-      },
-      bookingsServicesDetails: {
-        with: {
-          services: true,
-        },
-      },
-      manicurist: true,
-    },
+  const bookings = await getBookings({
+    offset: 0,
+    limit: 10,
+    day: now,
   });
 
-  const formattedBookings = bookings.map((booking) => {
-    return {
-      id: booking.id,
-      startTime: formatDateString(booking.startTime),
-      endTime: formatDateString(booking.endTime),
-      date: new Date(booking.startTime),
-      username: booking.name,
-      email: booking.email,
-      phone: booking.phone,
-      paymentStatus: formatPaymentStatus(booking.paymentStatus),
-      services: booking.bookingsServicesDetails.map(
-        (detail) => detail.services,
-      ),
-      totalPrice: booking.totalPrice,
-      advanceAmount: booking.advanceAmount,
-      extras: booking.bookingsExtrasDetails.map((detail) => detail.extras),
-      manicurist: booking.manicurist.username,
-    };
-  });
-
-  return formattedBookings;
+  return bookings;
 }
